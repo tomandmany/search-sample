cc37503b5857fc93b577a414faa7276d373c20db
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-06-20 14:32:09 +0900

    背景色の追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} bg-gray-200`}>
         <Header />
         {children}
         {/* <Footer /> */}
