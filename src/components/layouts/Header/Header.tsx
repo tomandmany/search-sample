d1007a33ac5dfdb1b319beaf10ba4af93089d011
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-07-19 14:08:12 +0900

    Headerに検索ページ（ルート）へのリンクを追加

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -2,10 +2,17 @@
 
 import ProgramDropdown from "./ProgramDropdown";
 import ParticipantDropdown from "./ParticipantDropdown";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header className="py-8 px-20 border-b-2 sticky top-0 bg-white space-x-10 flex z-10">
+            <Link
+                href={'/'}
+                className="hover:bg-slate-200 p-2 rounded transition flex justify-between"
+            >
+                検索ページ
+            </Link>
             <ProgramDropdown />
             <ParticipantDropdown />
         </header>
