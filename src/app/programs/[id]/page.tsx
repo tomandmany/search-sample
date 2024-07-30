d735a79972f9b441e9c9ea1aab001061472990f6
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-07-31 08:43:40 +0900

    /programsと/programs/[id]を単純に<h1>あ</h1>を表示するページに修正

--- src/app/programs/[id]/page.tsx
+++ src/app/programs/[id]/page.tsx
@@ -1,23 +1,9 @@
-// // パス: /app/page.tsx
+// パス: /app/page.tsx
 
-// import { getPrograms } from "@/data/programs";
-
-// export default async function Page({ params }: { params: { id: string } }) {
-//     const programId = params.id;
-//     const programs = await getPrograms();
-
-//     // currentprogramをフィルタリング
-//     const currentprogram = programs.find(program => program.id === programId);
-
-//     return (
-//         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//             {currentprogram ? (
-//                 <>
-//                     <h1 className="text-2xl font-bold">{currentprogram.name}</h1>
-//                 </>
-//             ) : (
-//                 <p>program not found</p>
-//             )}
-//         </main>
-//     );
-// }
+export default async function Page() {
+    return (
+        <>
+            <h1>あ</h1>
+        </>
+    );
+}
